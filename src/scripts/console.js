0e4c3f159f2c1836f7bd958e3fbce2045d613bc7
Gabe Ferreira <contact@gabeferreira.com>
2020-04-17 11:31:22 -0700

    Moved www to root
    Placed contents on the www folder on the root.

--- src/scripts/console.js
+++
@@ -1,4 +0,0 @@
-// Javascript Document
-
-console.log('Design and development by Gabe Ferreira');
-console.log('contact@gabeferreira.com');
--- src/scripts/preloader.js
+++
@@ -1,3 +0,0 @@
-// Javascript document
-
-// Preload all images seen when the site is first loaded
