de0c2c42b08375ee46f1598557e48bbe2b7b3709
josemeza <jose.meza.ramos@gmail.com>
2023-03-05 17:07:10 -0800

    Updated project descriptions

--- assets/db.js
+++ assets/db.js
@@ -2,7 +2,7 @@ projectDB = [
 	{
 		title:'The Most Searched',
 		id:'Most Searched',
-		description:'Art direction and design for a spotlight on some of the most searched moments and individuals in America in honor of Black History Month',
+		description:'Art direction and design for a Google spotlight on some of the most searched moments and individuals in America in honor of Black History Month',
 		images:[
 			'Google-0.gif',
 			'Google-2.jpg',
@@ -21,7 +21,7 @@ projectDB = [
 	{
 		title:'Space to Belong',
 		id:'Space to Belong',
-		description:'Art direction and design for a Pride season celebration of inclusive spaces that foster belonging for all',
+		description:'Art direction and design for a Google Pride season celebration of inclusive spaces that foster belonging for all',
 		images:[
 			'Pride - 1.jpg',
 			'Pride - 2.jpg',
@@ -67,7 +67,7 @@ projectDB = [
 	{
 		title:'IDEO',
 		id:'IDEO',
-		description:'Art direction for the design and consulting firm',
+		description:'Selected art direction for the design and consulting firm',
 		images:[
 			'IDEO-2.jpg',
 			'IDEO-3.jpg',
